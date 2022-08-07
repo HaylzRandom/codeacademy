ff2537fcce1a0c6a2fb3e5068061d79598a912fa
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-08-07 17:47:10 +0100

    Completed Intermediate React and React Router sections

---
+++ react/passing-thoughts/src/AddThoughtForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { generateId, getNewExpirationTime } from './utilities';
+
+export function AddThoughtForm(props) {
+	const [text, setText] = useState('');
+
+	const handleChange = (e) => {
+		setText(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (text.length > 0) {
+			const thought = {
+				id: generateId(),
+				text: text,
+				expiresAt: getNewExpirationTime(),
+			};
+
+			props.addThought(thought);
+			setText('');
+		}
+	};
+
+	return (
+		<form className='AddThoughtForm' onSubmit={handleSubmit}>
+			<input
+				type='text'
+				aria-label="What's on your mind?"
+				placeholder="What's on your mind?"
+				value={text}
+				onChange={handleChange}
+			/>
+			<input type='submit' value='Add' />
+		</form>
+	);
+}
